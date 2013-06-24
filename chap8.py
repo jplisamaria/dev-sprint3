9a27e7fd9ad1f4279fc03febe010b47b72185653
jplisamaria <jplisamaria@gmail.com>
2013-06-24 18:58:28 -0400

    ROT13--cleaned up

--- chap8.py
+++ chap8.py
@@ -2,6 +2,8 @@
 # Name:Lisa-Maria Mehta
 
 def rotate_letter (letter, n):
+	"""Takes a letter and an integer n as input, and returns letter
+	n places away from original letter."""
 	rotation_factor = n % 26
 	new_letter_ord = ord(letter) + rotation_factor
 	if letter.isupper():
@@ -18,24 +20,16 @@ def rotate_letter (letter, n):
 		return letter
 
 def rotate_word (word, n):
+	"""Takes a word and an integer n as input.  Calls rotate_letter
+	and returns a word with each letter n places away from
+	original letters."""
 	new_word = ""
 	for char in word:
 		new_word = new_word +rotate_letter(char, n)
 	return new_word
 
-def reverse (name):
-	nickname = ""
-	for i in range(len(name)):
-		nickname = nickname + name[-i-1]
-	return nickname
-
-def fix_capitalization():
-	pass
-
 print rotate_word('Cheer' , 7)
 print rotate_word('melon' , -10)
 print rotate_word('Melon.head' , -10)
 print rotate_word('sLEEp' , 9)
-print reverse ('aira-masil')
-
 
